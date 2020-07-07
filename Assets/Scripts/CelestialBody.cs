6e3fb77f75fb8e00d847c6ef419ecfd96b1c13c5
Jacob Preston <prestja@gmail.com>
2020-07-07 08:13:41 -0500

    Basic orbital parameters

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour {
+    [SerializeField] private CelestialBody parent;
+    // classical parameters
+    [SerializeField] private float t0; // temporal offset
+    [SerializeField] private float m0; // mean motion at t0
+    [SerializeField] private float semiMajorAxis;
+    [SerializeField] private float orbitalPeriod;
+    
+    private float MeanMotion {
+        get {
+            return (2 * Mathf.PI) / orbitalPeriod;
+        }
+    }
+
+    private float MeanAnomaly(float t) {
+        return m0 + MeanMotion * (t - t0);
+    }
+
+    private void Update() {
+        if (!parent) // stars or barycenters should not appear to move
+            return;
+        Debug.Log(MeanAnomaly(Time.time));
+    }
+}

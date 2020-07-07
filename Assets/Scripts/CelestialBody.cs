3c94cab0c5bcca6f17e48b4cbf9b04932ebae42e
Jacob Preston <prestja@gmail.com>
2020-07-07 08:36:52 -0500

    Calculate eccentric anomaly

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -9,7 +9,8 @@ public class CelestialBody : MonoBehaviour {
     [SerializeField] private float m0; // mean motion at t0
     [SerializeField] private float semiMajorAxis;
     [SerializeField] private float orbitalPeriod;
-    
+    [SerializeField] private float eccentricity;
+
     private float MeanMotion {
         get {
             return (2 * Mathf.PI) / orbitalPeriod;
@@ -20,9 +21,27 @@ public class CelestialBody : MonoBehaviour {
         return m0 + MeanMotion * (t - t0);
     }
 
+    private float EccentricAnomaly(float t) {
+        float anomaly = eccentricity < 0.08 ? MeanAnomaly(t) : Mathf.PI;
+        float anomalyPrev = (MeanAnomaly(t) + Mathf.PI) * 4.0f;
+
+        int itr = 0;
+        while (Mathf.Abs(anomaly - anomalyPrev) > 1E-12) {
+            itr++;
+            if (itr > 1000)
+                break;
+            anomalyPrev = anomaly;
+            anomaly = anomaly -
+                (anomaly - eccentricity * Mathf.Sin(anomaly) - MeanAnomaly(t)) /
+                (1 - eccentricity * Mathf.Cos(anomaly));
+        }
+        return anomaly;
+    }
+
     private void Update() {
         if (!parent) // stars or barycenters should not appear to move
             return;
-        Debug.Log(MeanAnomaly(Time.time));
+        //Debug.Log(MeanAnomaly(Time.time));
+        Debug.Log(EccentricAnomaly(Time.time));
     }
 }
